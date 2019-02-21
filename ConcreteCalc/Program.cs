030d338bf2ead037e5d42f5e5b90481ea5721dd6
sergeymaguire <sergeymaguire@gmail.com>
2019-02-20 16:38:00 -0800

    Concrete calculator complete

--- ConcreteCalc/Program.cs
+++ ConcreteCalc/Program.cs
@@ -10,7 +10,38 @@ namespace ConcreteCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hi");
+            double dYdsOrd = getYardsOrdered();
+
+            double dNetBill = CalcBill(dYdsOrd);
+            Console.WriteLine("The net bill is: " + "$" + dNetBill);
+            Console.ReadLine();
+
+        }
+        private static double CalcBill(double pdYdsOrd)
+        {
+            if (pdYdsOrd >= 8)
+            {
+                //Console.WriteLine("Your net bill is " + "$" + dNetBillScr);
+                return 49.50 * pdYdsOrd;
+            }
+            else if (pdYdsOrd <= 3)
+            {
+                return pdYdsOrd * 55.50 + 25.00;
+            }
+            else
+            {
+                return pdYdsOrd * 55.50;
+            }
+
+        }
+
+        private static double getYardsOrdered()
+        {
+            double dYdsOrd;
+            Console.Write("Enter how much concrete you would like to buy: ");
+            dYdsOrd = Double.Parse(Console.ReadLine());
+            return dYdsOrd;
+            //string sYdsOrd = Console.ReadLine();  
         }
     }
 }
